afc43af87e970cd67f28e41a54e916b05e9958b0
Raphael Viana <45907874+rnvdev@users.noreply.github.com>
2020-08-02 21:13:05 -0300

    Create float_input_to_int.go

---
+++ float_input_to_int.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+  var response float64
+	fmt.Scanln(&response)
+  forDecimal(response)
+}   
+    
+func forDecimalN(x float64){
+  fmt.Printf("%d", int16(x))
+};
