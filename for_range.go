5505f8789c5ba3d54d3559c29a2fcca5e68b82b1
Raphael Viana <45907874+rnvdev@users.noreply.github.com>
2020-08-03 22:18:04 -0300

    Update for_range.go

--- for_range.go
+++ for_range.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 func main() {
-	x := [3]int{1, 2, 3}
-	for i, v:= range x {
-	    fmt.Printf("index: %d | value: %d \n",i,v);
-	}
+    x := [3]int{1, 2, 3}
+    for i, v:= range x {
+        fmt.Printf("index: %d | value: %d \n",i,v);
+    }
 }
+
+/*
+output:
+index: 0 | value: 1 
+index: 1 | value: 2 
+index: 2 | value: 3
+*/
